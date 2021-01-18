cfdcfe1459a4fdd77832da4dc552715af7e09267
Shenglong Wang <wangsl2001@gmail.com>
2021-01-18 09:04:45 -0500

    adjust memory

--- greeneCPU.lua
+++ greeneCPU.lua
@@ -19,9 +19,11 @@ local ave_memory = 0
 local function available_partitions()
    local partitions = nil
    if greeneCommon.is_interactive_job() then
-      partitions = { "cs", "cm", "cpu_gpu", "cl" }
+      -- partitions = { "cs", "cm", "cpu_gpu", "cl" }
+      partitions = { "cl", "cm", "cs", "cpu_gpu" }
    else
-      partitions = { "cs", "cm", "cl" }
+      --partitions = { "cs", "cm", "cl" }
+      partitions = { "cl", "cm", "cs" }
    end
    return partitions
 end
@@ -31,19 +33,19 @@ local partition_configurations = {
    cs =  { min_cpus = 1, max_cpus = 48,
 	   max_nodes = 524,
 	   min_memory = 0, max_memory = 180,
-	   min_ave_memory = 0, max_ave_memory = 30, 
+	   min_ave_memory = 0, max_ave_memory = 32, 
    },
    
    cm =  { min_cpus = 1, max_cpus = 48,
 	   max_nodes = 40,
 	   min_memory = 10, max_memory = 369,
-	   min_ave_memory = 3, max_ave_memory = 60
+	   min_ave_memory = 3, max_ave_memory = 128
    },
    
    cl =  { min_cpus = 1, max_cpus = 96,
 	   max_nodes = 4,
-	   min_memory = 64, max_memory = 3014,
-	   min_ave_memory = 15, max_ave_memory = 3014
+	   min_memory = 30, max_memory = 3014,
+	   min_ave_memory = 30, max_ave_memory = 3014
    },
 
    cpu_gpu =  { min_cpus = 1, max_cpus = 20,
